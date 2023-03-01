fea274639eb7fb6e4d47a76852c26047a6e4f278
Heitor458 <heitormota16@gmail.com>
2023-03-01 16:37:13 -0300

    Commit

--- JS/main.js
+++ JS/main.js
@@ -15,7 +15,7 @@ $(document).ready(function (){
      var options = {
         strings : ['Web Developer', 'Game dev', 'Designer 3D', 'QA'],
         typeSpeed: 90,
-        loop: true
+        loop: false
      };
      var typed = new Typed('.element', options)
 
